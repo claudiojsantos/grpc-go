c07cb1f918684dc7bed2fbe381aa12c1f1d7ae4e
Cláudio Santos <claudio@sistnet.com.br>
2023-11-26 10:46:13 -0300

    file generated by protoc

---
+++ internal/service/category.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"context"
+
+	"github.com/claudiojsantos/grpc-go/internal/database"
+	"github.com/claudiojsantos/grpc-go/internal/pb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{CategoryDB: categoryDB}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(in.Nome, in.Descricao)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return &pb.CategoryResponse{
+		Category: categoryResponse,
+	}, nil
+}
